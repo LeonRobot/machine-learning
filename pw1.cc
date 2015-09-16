b8bc46f18c5712185a9160b91c0fafa26a6a2c65
jakeyeung <jakeyeung@gmail.com>
2015-09-16 13:10:01 +0000

    Plots training data but regressor isnt work

--- pw1.cc
+++ pw1.cc
@@ -40,6 +40,7 @@ void example() {
   for(int k = 0; k < nb_train_samples; k++) {
     x_train[k] = random_0_to_1() * 2 * M_PI - M_PI;
     y_train[k] = sin(x_train[k]);
+		// printf("X_traink: %4.2f", x_train[k]);
   }
 
   PolynomialRegressor regressor(degree);
@@ -54,6 +55,8 @@ void example() {
   ofstream out("/tmp/example.dat");
 
   for(int s = 0; s < nb_test_values; s++) {
+		// printf("x: %4.2f s: %i s/nb: %4.2f", x, s, scalar_t(x) / scalar_t(nb_test_values);
+		// printf("x: %4.2f s: %i s/nb: %4.2f\n", x, s, scalar_t(s) / scalar_t(nb_test_values));
     scalar_t x = scalar_t(s) / scalar_t(nb_test_values) * 2 * M_PI - M_PI;
     out << x << " " << sin(x) << " " << regressor.eval(x) << endl;
   }
@@ -63,6 +66,46 @@ void example() {
 }
 
 void question1() {
+	// printf("Hello World!\n");
+	
+	int degree = 4;
+ 	int nb_train_samples = 100;
+	int nb_test_values = 1000;
+
+ 	scalar_t *x_train = new scalar_t[nb_train_samples];
+	scalar_t *y_train = new scalar_t[nb_train_samples];
+
+//	printf(nb_train_samples)
+
+  for(int k = 0; k < nb_train_samples; k++){
+  	x_train[k] = random_0_to_1();
+  if (x_train[k] < 0.5){
+  	y_train[k] = 0;
+  } 
+	else {
+  	y_train[k] = 1;
+  }
+	// printf("X, Y: %4.2f %4.2f\n", x_train[k], y_train[k]);
+  }	
+
+  PolynomialRegressor regressor(degree);
+  regressor.fit(nb_train_samples, x_train, y_train);
+
+  ofstream out_train("/tmp/question1_train.dat");
+
+  for(int s = 0; s < nb_train_samples; s++) {
+    out_train << x_train[s] << " " << y_train[s] << endl;
+  }
+
+  ofstream out("/tmp/question1.dat");
+
+  for(int s = 0; s < nb_test_values; s++) {
+    scalar_t x = scalar_t(s) / scalar_t(nb_test_values);
+		out << degree << " " << x << regressor.eval(x) << endl;
+  }
+
+  delete[] x_train;
+  delete[] y_train;
 }
 
 void question2() {
